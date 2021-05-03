4729ccb1928c5a13e8664a22eff3a1a9b6ee9814
Kitty Xu <kittyxu725@gmail.com>
2021-05-03 18:23:28 -0400

    feature: search bar component

---
+++ dist/main.tsx
@@ -0,0 +1,59 @@
+const app = document.querySelector("#App") as HTMLTextAreaElement;
+
+type MyProps = {
+  message: string;
+};
+type MyState = {
+  [key: string]: string;
+};
+class App extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+      inputValue: "Search..",
+      dropDownValue: "Deliver now",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (event: any) => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event: any) => {
+    const time =
+      this.state.dropDownValue === "Deliver now"
+        ? " for delivery now"
+        : " for delivery later";
+    app.textContent = "You searched " + this.state.inputValue + time;
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form id="search-bar" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="inputValue"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+          />
+          <select
+            name="dropDownValue"
+            value={this.state.dropDownValue}
+            onChange={this.handleChange}
+          >
+            <option value="deliver_now">Deliver now</option>
+            <option value="schedule_for_later">Schedule for later</option>
+          </select>
+          <input type="submit" value="Find Food" />
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App), app);
---
+++ public/bundle.js
@@ -0,0 +1,11 @@
+(() => {
+  var e;
+  document.body.appendChild(
+    (((e = document.createElement("div")).innerHTML = _.join(
+      ["Hello", "webpack"],
+      " "
+    )),
+    e)
+  );
+})();
+//# sourceMappingURL=bundle.js.map
